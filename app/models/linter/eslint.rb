2c41800ba3c8d00c635410aafe7900742b19d8ae
Dmitry Golyshev <dmitry@goodeggs.com>
2016-02-29 13:50:34 -0800

    Make ESLint default js linter by Rgex

--- app/models/linter/eslint.rb
+++ app/models/linter/eslint.rb
@@ -1,5 +1,6 @@
 module Linter
   class Eslint < Base
-    FILE_REGEXP = /.+(\.jsx?|\.es6|\.es6|\.js)\z/
+    FILE_REGEXP = /.+\.js\z/
+    # FILE_REGEXP = /.+(\.jsx?|\.es6|\.es6|\.js)\z/
   end
 end
